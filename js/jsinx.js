0b5df019be7f54966ff47288f2c934a01fdad52d
corralesmikel <corrales.mikel@uni.eus>
2023-10-30 14:18:03 +0100

    version 10

--- js/jsinx.js
+++ js/jsinx.js
@@ -1,13 +1,38 @@
 document.addEventListener('DOMContentLoaded', function (event) {
-    //Noticias();
-    Login();
+    Noticias();
+    //Login();
 });
-/*
+
 function Noticias() {
+    $.ajax({
+        url: './json/api.json',
+        dataType: 'json', //specifying here the response type, there's no need to parse the response
+        success: function (response) {
+            //Variable
+            var myhtml = "";
+            //Loop
+            for (var i = 0; i < response.data.length; i++) {
+                myhtml += '<div class="Item-Flex">';
+                myhtml += "<img src='" + response.data[i].avatar + "'>";
+                myhtml += '<p>' + response.data[i].first_name + '</p>';
+                myhtml += '<p>' + response.data[i].email + '</p>';
+                myhtml += '</div>';
+            }
+            //ID Text
+            document.getElementById("Main-ID").innerHTML = myhtml;
+        },
+        error: function (xhr) {
+            alert("An AJAX error occured: " + xhr.status + " " + xhr.statusText);
+        }
+    });
+}
+
+/*
+function Login() {
     var datos = {
         'Titulo': Titulo,
         'Subtitulo': Subtitulo,
-        ' Imagenes':  Imagenes
+        'Imagenes': Imagenes
     };
 
 
@@ -18,7 +43,7 @@ function Noticias() {
         dataType: 'json',
 
         success: function (response) { // response contiene la respuesta del server
-            alert (response.status); 
+            alert(response.status);
         },
         error: function (xhr) {
             console.error(xhr.responseText);
@@ -26,6 +51,3 @@ function Noticias() {
     });
 }
 */
-function Login() {
-    
-}
